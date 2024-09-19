440d8091bac7bed90f0be2f5cd68d3dabdd488bf
Mayboyzz <mayboyz99@gmail.com>
2024-09-19 17:16:06 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,6 +39,21 @@ router.post("/", async (req, res, next) => {
 	});
 });
 
+// Restore session user
+router.get("/", (req, res) => {
+	const { user } = req;
+	if (user) {
+		const safeUser = {
+			id: user.id,
+			email: user.email,
+			username: user.username,
+		};
+		return res.json({
+			user: safeUser,
+		});
+	} else return res.json({ user: null });
+});
+
 // Log out
 router.delete("/", (_req, res) => {
 	res.clearCookie("token");
