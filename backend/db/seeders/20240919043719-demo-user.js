5cc096aa0e8c42dbcdce68f80b224993198255c5
selin <uzunogluselin92@gmail.com>
2024-09-19 18:18:54 -0500

    finished the phase-5

--- backend/db/seeders/20240919043719-demo-user.js
+++ backend/db/seeders/20240919043719-demo-user.js
@@ -16,16 +16,22 @@ module.exports = {
 					email: "demo@user.io",
 					username: "Demo-lition",
 					hashedPassword: bcrypt.hashSync("password"),
+					firstName: "demo-Name",
+					lastName: "demo-Lastname",
 				},
 				{
 					email: "user1@user.io",
 					username: "FakeUser1",
 					hashedPassword: bcrypt.hashSync("password2"),
+					firstName: "demo-Name",
+					lastName: "demo-Lastname",
 				},
 				{
 					email: "user2@user.io",
 					username: "FakeUser2",
 					hashedPassword: bcrypt.hashSync("password3"),
+					firstName: "demo-Name",
+					lastName: "demo-Lastname",
 				},
 			],
 			{ validate: true }
