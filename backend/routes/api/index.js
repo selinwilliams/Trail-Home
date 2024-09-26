3c5885fd47bd03d6aaa00fa136b2136e2e8c48be
Mayboyzz <mayboyz99@gmail.com>
2024-09-26 12:37:03 -0500

    Add delete review image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const reviewsRouter = require("./reviews.js");
 const bookingsRouter = require("./bookings.js");
 const { restoreUser } = require("../../utils/auth.js");
 const spotImageRouter = require("./spot-images.js");
+const reviewImageRouter = require("./review-images.js");
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -18,6 +19,7 @@ router.use("/spots", spotsRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/bookings", bookingsRouter);
 router.use("/spot-images", spotImageRouter);
+router.use("/review-images", reviewImageRouter);
 router.post("/test", (req, res) => {
 	res.json({ requestBody: req.body });
 });
--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { Spot, User, SpotImage, Review, Booking } = require("../../db/models");
+const {
+	Spot,
+	User,
+	SpotImage,
+	Review,
+	Booking,
+	ReviewImage,
+} = require("../../db/models");
 const router = express.Router();
 
 //Delete a Spot Image
@@ -8,17 +15,17 @@ router.delete("/:imageId", async (req, res) => {
 	const { imageId } = req.params;
 
 	if (user) {
-		const spotImage = await SpotImage.findByPk(imageId);
-		if (spotImage) {
-			const spot = await Spot.findByPk(spotImage.spotId);
-			if (user.id === spot.ownerId) {
-				await spotImage.destroy();
+		const reviewImage = await ReviewImage.findByPk(imageId);
+		if (reviewImage) {
+			const review = await Review.findByPk(reviewImage.reviewId);
+			if (user.id === review.userId) {
+				await reviewImage.destroy();
 				res.status(200).json({ message: "Successfully deleted" });
 			} else {
 				res.status(403).json({ message: "Forbidden" });
 			}
 		} else {
-			res.status(404).json({ message: "Spot Image couldn't be found" });
+			res.status(404).json({ message: "Review Image couldn't be found" });
 		}
 	} else {
 		res.status(401).json({ message: "Authentication required" });
