fc7ed29c0691b8feb4e21acba0aedc3dfed6455c
Mayboyzz <mayboyz99@gmail.com>
2024-09-26 10:20:09 -0500

    Fix foreign key for spotId, UserId

--- backend/db/migrations/20240924173033-create-booking.js
+++ backend/db/migrations/20240924173033-create-booking.js
@@ -7,57 +7,59 @@ if (process.env.NODE_ENV === "production") {
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(
-      "Bookings",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        spotId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: "Spots",
-            key: "id",
-          },
-        },
-        userId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: "Users",
-            key: "id",
-          },
-        },
-        startDate: {
-          type: Sequelize.DATE,
-        },
-        endDate: {
-          type: Sequelize.DATE,
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          type: Sequelize.DATE,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      },
-      options
-    );
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Bookings");
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(
+			"Bookings",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				spotId: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					references: {
+						model: "Spots",
+						key: "id",
+					},
+					onDelete: "CASCADE",
+				},
+				userId: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					references: {
+						model: "Users",
+						key: "id",
+					},
+					onDelete: "CASCADE",
+				},
+				startDate: {
+					type: Sequelize.DATE,
+				},
+				endDate: {
+					type: Sequelize.DATE,
+				},
+				createdAt: {
+					type: Sequelize.DATE,
+				},
+				updatedAt: {
+					type: Sequelize.DATE,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+			},
+			options
+		);
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Bookings");
+	},
 };
