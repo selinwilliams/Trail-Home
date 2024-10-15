4f41e36880d1a63deeee6f0a082c82a6e69ce411
selin <uzunogluselin92@gmail.com>
2024-10-15 11:59:42 -0500

    did signup model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import * as sessionActions from './store/session';
-import SignupFormPage from './components/SignupFormPage';
 import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -17,7 +16,7 @@ function Layout() {
 
   return (
     <>
-    <Navigation isLoaded={isLoaded}/>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
@@ -30,10 +29,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: '/signup',
-        element: <SignupFormPage/>
       }
     ]
   }
